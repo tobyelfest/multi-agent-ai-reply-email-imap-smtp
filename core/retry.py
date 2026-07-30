ee84df417e9a4eb571d10e2a5fee066bbc075128
tobyelfest <tobyelfest@gmail.com>
2026-07-30 21:36:38 +0700

    Fix formatting and remove extra import in retry.py

--- core/retry.py
+++ core/retry.py
@@ -3,7 +3,7 @@
 import time
 import logging
 from functools import wraps
-from typing import Callable, Type, Tuple, Optional, Any
+from typing import Callable, Type, Tuple, Optional
 
 
 class RetryError(Exception):
